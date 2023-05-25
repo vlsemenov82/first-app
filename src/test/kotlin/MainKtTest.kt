b3fb67cba1715336ed09d9d11e50eebdf226af41
Vladimir Semenov <vl.semenov82@yandex.ru>
2023-05-25 17:42:24 +0300

    Task 4-2 Autotest false

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -60,7 +60,7 @@ class MainKtTest {
     @Test
     fun visaComissionNoLimit() {
         val result = paymentComission("Visa", 0, 10000)
-        assertEquals(75, result)
+        assertEquals(750, result)
     }
 
     @Test
